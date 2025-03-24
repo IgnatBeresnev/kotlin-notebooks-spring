e7f7814e1cf0721fc223aa4b75c4001137f9f476
IgnatBeresnev <ignat.beresnev@jetbrains.com>
2025-03-24 14:48:26 +0100

    Update Kotlin to 2.0 + upgrade Gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,9 +2,9 @@ plugins {
     id("org.springframework.boot") version "3.2.2"
     id("io.spring.dependency-management") version "1.1.4"
 
-    kotlin("jvm") version "1.9.0"
-    kotlin("plugin.spring") version "1.9.0"
-    kotlin("plugin.jpa") version "1.9.0"
+    kotlin("jvm") version "2.0.0"
+    kotlin("plugin.spring") version "2.0.0"
+    kotlin("plugin.jpa") version "2.0.0"
 }
 
 group = "com.example"
